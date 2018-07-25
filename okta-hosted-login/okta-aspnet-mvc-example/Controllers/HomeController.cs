fd7968465aa072901e6ba135f8996af6e7218666
Laura Rodriguez <rdz.maria.laura@gmail.com>
2018-07-25 14:33:37 -0400

    MVC working project.

---
+++ okta-hosted-login/okta-aspnet-mvc-example/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace okta_aspnet_mvc_example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [Authorize]
+        public ActionResult Profile()
+        {
+            return View(HttpContext.GetOwinContext().Authentication.User.Claims);
+        }
+    }
+}
