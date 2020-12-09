3ec8033c3f0507e94fdae9ec99e5f24a5f60127a
Andrii Zhegurov <andrii.zhegurov@okta.com>
2020-12-09 22:30:46 +0200

    Additional commit

--- primary-auth/okta-aspnet-mvc-example/Properties/AssemblyInfo.cs
+++ primary-auth/okta-aspnet-mvc-example/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("okta_aspnet_mvc_example")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Okta ASP.NET Example")]
+[assembly: AssemblyDescription("ASP.NET MVC & Okta Authentication SDK Example")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("okta_aspnet_mvc_example")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
