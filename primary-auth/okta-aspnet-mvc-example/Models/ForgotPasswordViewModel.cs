2e25a19096db19bd98e19f674e5c423f59c8ee87
Laura Rodriguez <rdz.maria.laura@gmail.com>
2021-10-18 15:51:38 -0400

    Minor change.

--- primary-auth/okta-aspnet-mvc-example/Models/ForgotPasswordViewModel.cs
+++ primary-auth/okta-aspnet-mvc-example/Models/ForgotPasswordViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace okta_aspnet_mvc_example.Models
 {
     public class ForgotPasswordViewModel
     {
+        [Required]
         public string UserName { get; set; }
     }
 }
